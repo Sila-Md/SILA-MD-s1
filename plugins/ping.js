80b1dcf687c55e06a62d1c4b2ed6484310f047fc
Sila-Md <hackersila2@gmail.com>
2026-05-18 20:32:26 +0300

    𝐒𝐢𝐥𝐚 𝐓𝐞𝐜𝐡🤓

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,18 +1,31 @@
+// plugins/ping.js
 module.exports = {
-  command: "ping",
-  desc: "Check bot response time",
-  category: "utility",
-  use: ".ping",
-  fromMe: false,
-  filename: __filename,
+    command: 'ping',
+    description: 'Check bot response time',
+    alias: ['pong', 'latency'],
+    category: 'utility',
+    async execute(socket, msg, args, botNumber) {
+        const from = msg.key.remoteJid;
+        
+        // Send initial message
+        const sentMsg = await socket.sendMessage(from, { 
+            text: '*_⚡️ ᴘɪɴɢɪɴɢ ᴛᴏ sᴇʀᴠᴇʀ..._*' 
+        }, { quoted: msg });
+        
+        const startTime = Date.now();
+        const endTime = Date.now();
+        const ping = endTime - startTime;
+        
+        const pingMsg = `*╭━━〔 🐢 ᴘɪɴɢ 🐢 〕━━┈⊷*
+*┃🐢│ • 🏓 ᴘᴏɴɢ!*
+*┃🐢│ • ⚡ ʟᴀᴛᴇɴᴄʏ: ${ping}ms*
+*╰━━━━━━━━━━━━━━━┈⊷*
 
-  execute: async (sock, msg) => {
-    const start = Date.now();
-    await sock.sendMessage(msg.key.remoteJid, { text: "*GG...🥺*" });
-    const latency = Date.now() - start;
-    
-    await sock.sendMessage(msg.key.remoteJid, { 
-      text: `*PONG...😊*` 
-    }, { quoted: msg });
-  }
+> 🐢 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 𝐒𝐢𝐥𝐚*`;
+        
+        await socket.sendMessage(from, { 
+            text: pingMsg, 
+            edit: sentMsg.key 
+        });
+    }
 };
